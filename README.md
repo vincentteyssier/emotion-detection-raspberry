451c991973c05bd18fa37c67d07ecc55c0f916ed
vincentteyssier <v.teyssier@gmail.com>
2018-04-02 15:50:14 +0200

    Full data flow implemented

--- README.md
+++ README.md
@@ -91,4 +91,88 @@ This captures a screenshot 2s after activation of the camera then close the vide
 ## OpenCV
 
 We use the HaarCascade Classifier to find out faces in the video stream.
-On this first version we loop until 20 faces are detected. Each bounding box is saved as an image file and then fed to the TF model.
+A future optimization would be to use the new OpenCV DNN Classifier.
+
+First we instantiate the picamera and give it time to start
+```
+# instanciate the camera
+camera = PiCamera()
+camera.resolution = (1920, 1080)
+camera.framerate = 30
+rawCapture = PiRGBArray(camera, size=(1920, 1080))
+# allow the camera to warmup
+time.sleep(0.1)
+```
+
+We then load our labels and the previously trained model:
+
+```
+# Loads label file, strips off carriage return
+label_lines = [line.rstrip() for line 
+                   in tf.gfile.GFile("./retrained_data/retrained_labels.txt")]
+# load our pretrained model
+with tf.gfile.FastGFile("./retrained_data/retrained_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+```
+
+Then we need to load the HaarCascade Classifier from the opencv models library. I've included the xml to this repo.
+
+'faceDetect = cv2.CascadeClassifier('./retrained_data/haarcascade_frontalface_default.xml')'
+
+Then the streaming loop is quite self-explanatory. We create the tf session, start the streaming in raw format from the picamera, display it, transform to grey scale, use opencv `detectMultiScale` to return the detected faces. We then transform the grayed image to a numpy array and for each face detected feed the cropped data to the tf graph. We then run our prediction and display the highest probability class label.
+
+When the user press 'q' the demo ends.
+
+```
+# start the tensorflow session and start streaming and image processing
+with tf.Session() as sess:
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+    # capture frames from the camera
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        
+        # show the frame
+        cv2.imshow("face", frame)
+
+        # transform to Gray scale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # detect faces in our gray picture
+        faces = faceDetect.detectMultiScale(gray,
+                                            scaleFactor=1.3,
+                                            minNeighbors=5
+                                            )
+
+        # transform into a numpy array for tf processing
+        gray_np = gray.array
+        
+        for (x,y,w,h) in faces:
+            #sampleNum = sampleNum+1
+            #cv2.imwrite("./temp_dataset/"+str(sampleNum)+".jpg", gray[y:y+h,x:x+w])
+            
+            # feed the detected face (cropped image) to the tf graph
+    		predictions = sess.run(softmax_tensor, {'DecodeJpeg:0': gray_np[y:y+h,x:x+w]})
+            prediction = predictions[0]
+
+            # Get the highest confidence category.
+            prediction = prediction.tolist()
+            max_value = max(prediction)
+            max_index = prediction.index(max_value)
+            predicted_label = label_lines[max_index]
+
+            print("%s (%.2f%%)" % (predicted_label, max_value * 100))
+
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.waitKey(100);
+
+        key = cv2.waitKey(1) & 0xFF
+    
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+    
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            cv2.destroyAllWindows()
+            break
+```
--- emotion_detection_v1.py
+++ emotion_detection_v1.py
@@ -11,72 +11,76 @@ camera = PiCamera()
 camera.resolution = (1920, 1080)
 camera.framerate = 30
 rawCapture = PiRGBArray(camera, size=(1920, 1080))
-
 # allow the camera to warmup
 time.sleep(0.1)
 
-# We use the Haar Cascade classifier
-faceDetect = cv2.CascadeClassifier('./retrained_data/haarcascade_frontalface_default.xml')
-
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
- 
-	# show the frame
-	cv2.imshow("face", image)
-
-    # transform to Gray scale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # detect faces in our gray picture
-    faces = faceDetect.detectMultiScale(gray,
-                                          scaleFactor=1.3,
-                                          minNeighbors=5
-                                          )
-
-    for (x,y,w,h) in faces:
-        #sampleNum = sampleNum+1
-        #cv2.imwrite("./temp_dataset/"+str(sampleNum)+".jpg", gray[y:y+h,x:x+w])
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        cv2.waitKey(100);
-
-	key = cv2.waitKey(1) & 0xFF
- 
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
- 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		cv2.destroyAllWindows()
-        break
-
-
-
-
-"""
 # Loads label file, strips off carriage return
 label_lines = [line.rstrip() for line 
                    in tf.gfile.GFile("./retrained_data/retrained_labels.txt")]
-
+# load our pretrained model
 with tf.gfile.FastGFile("./retrained_data/retrained_graph.pb", 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
 
+# We use the Haar Cascade classifier
+faceDetect = cv2.CascadeClassifier('./retrained_data/haarcascade_frontalface_default.xml')
 
-# feed the 20 tf images to our tensorflow model
-i = 1
-while ( i <= 20):	
-	#gets the images one by one	
-	image_path = "./temp_dataset/"+str(i)+".jpg"
-	image_data = tf.gfile.FastGFile(image_path, 'rb').read()
-	
-	with tf.Session() as sess:
-    		# Feed the image_data as input to the graph and get first prediction
-    		softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+# start the tensorflow session and start streaming and image processing
+with tf.Session() as sess:
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+    # capture frames from the camera
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        
+        # show the frame
+        cv2.imshow("face", frame)
+
+        # transform to Gray scale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # detect faces in our gray picture
+        faces = faceDetect.detectMultiScale(gray,
+                                            scaleFactor=1.3,
+                                            minNeighbors=5
+                                            )
+
+        # transform into a numpy array for tf processing
+        gray_np = gray.array
+        
+        for (x,y,w,h) in faces:
+            #sampleNum = sampleNum+1
+            #cv2.imwrite("./temp_dataset/"+str(sampleNum)+".jpg", gray[y:y+h,x:x+w])
+            
+            # feed the detected face (cropped image) to the tf graph
+    		predictions = sess.run(softmax_tensor, {'DecodeJpeg:0': gray_np[y:y+h,x:x+w]})
+            prediction = predictions[0]
+
+            # Get the highest confidence category.
+            prediction = prediction.tolist()
+            max_value = max(prediction)
+            max_index = prediction.index(max_value)
+            predicted_label = label_lines[max_index]
+
+            print("%s (%.2f%%)" % (predicted_label, max_value * 100))
+
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.waitKey(100);
+
+        key = cv2.waitKey(1) & 0xFF
     
-    		predictions = sess.run(softmax_tensor, \
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+    
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            cv2.destroyAllWindows()
+            break
+
+
+
+
+"""
+       		predictions = sess.run(softmax_tensor, \
              		{'DecodeJpeg/contents:0': image_data})
     
     		# Sort to show labels of first prediction in order of confidence
@@ -86,6 +90,4 @@ while ( i <= 20):
         		human_string = label_lines[node_id]
         		score = predictions[0][node_id]
         		print('%s (score = %.5f)' % (human_string, score))
-	i = i +1
-
 """
