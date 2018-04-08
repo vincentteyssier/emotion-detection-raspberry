fdef0bb1ec059c7c061674b9c21724acd5a65065
vincentteyssier <v.teyssier@gmail.com>
2018-04-08 20:44:00 +0200

    .

--- dataset_augmentation_scripts/googleimage_scraper.py
+++ dataset_augmentation_scripts/googleimage_scraper.py
@@ -3,6 +3,7 @@ import json
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome(r"C:\Program Files\Google\chromedriver.exe")
 import os
 import urllib
 
